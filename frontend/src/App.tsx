fc39b88547a385df4322d98ad221ab0a1d49ea85
ming <elricym@outlook.com>
2023-04-08 14:02:17 +0800

    feat: Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,12 @@ import Lock from './components/Lock';
 import Setup from './components/Setup';
 import VaultList from './components/VaultList';
 import { StorageKeys } from './constants/keys';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
 
 function App() {
   const { connectAsync, connectors } = useConnect();
-  const { disconnectAsync } = useDisconnect();
-  const { address, isConnected } = useAccount();
+  const { isConnected } = useAccount();
   const [locked, setLocked] = useState(true);
   const [initialized] = useLocalStorageState(StorageKeys.vaultSetupFinished, {
     defaultValue: false,
@@ -18,42 +18,7 @@ function App() {
 
   return (
     <div className="App relative flex flex-col">
-      <header className="h-20">
-        {isConnected && (
-          <div className="flex w-full py-2 px-4 items-center">
-            <div className="flex items-center grow">
-              <img alt="" src="img/logo.svg" width={40} height={40} />
-              <p className="text-xs text-slate-200">
-                {address?.substring(0, 5) + '..' + address?.substring(38)}
-              </p>
-            </div>
-
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="w-10 h-10 text-slate-200 btn p-0 btn-circle btn-sm">
-                <img
-                  alt=""
-                  src="img/ic-setting.svg"
-                  width={16}
-                  height={16}
-                  className="text-white"
-                />
-              </label>
-
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <div onClick={() => disconnectAsync()}>Disconnect</div>
-                </li>
-                <li>
-                  <div onClick={() => setLocked(true)}> Lock</div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        )}
-      </header>
+      <Header isConnected={isConnected} setLocked={setLocked} />
       <header className="App-header">
         {isConnected && !initialized && <Setup />}
         {isConnected && locked && initialized && <Lock setUnlock={() => setLocked(false)} />}
